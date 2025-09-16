7f2018fa366e0b96555d6e3763478b78c9383ad0
Mizzage ♡🎀 <live2dloreamstudios@gmail.com>
2025-09-16 16:28:55 +0200

    Update script.js

--- asset/script.js
+++ asset/script.js
@@ -5,7 +5,7 @@ const fileInput = document.querySelector("#file-input");
 const fileUploadWrapper = document.querySelector(".file-upload-wrapper");
 const fileCancelButton = fileUploadWrapper.querySelector("#file-cancel");
 const closeChatbot = document.querySelector("#close-chatbot");
-// API setup, please use base64 to decode the API key: QUl6YVN5QXRoZ1dseGw0QWV4d1E2Vjc2Y21JYUtpMktIeVc4d0VZ
+// API setup, please use base64 to decode the API key: QUl6YVN5QXRoZ1dseGw0QWV4d1E2Vjc2Y21JYUtpMktIeVc4d0VZ -> https://www.base64encode.org/
 const API_KEY = "";
 const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`;
 // Initialize user message and file data
@@ -157,3 +157,4 @@ document.querySelector("#file-upload").addEventListener("click", () => fileInput
 closeChatbot.addEventListener("click", () => document.body.classList.remove("show-chatbot"));
 
 chatbotToggler.addEventListener("click", () => document.body.classList.toggle("show-chatbot"));
+
